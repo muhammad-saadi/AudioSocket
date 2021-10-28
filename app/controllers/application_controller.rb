d7b11376c9b81c771ccc2cf21ebce8844c0bbd00
muhammad-saadi <muhammad.saad@square63.org>
2021-10-28 16:38:36 -0700

    user_role integration and sign_up and sign_in styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_params , if: :devise_controller?
+
   def after_sign_in_path_for(resource_or_scope)
-      edit_user_registration_path
+    edit_user_registration_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
-      new_user_session_path
-    end
+    new_user_session_path
+  end
+
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys:[:role])
+   end
+
 end
