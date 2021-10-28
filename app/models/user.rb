6c30835573b2f7e009b619b20e9599e0327af68b
muhammad-saadi <muhammad.saad@square63.org>
2021-10-28 16:38:36 -0700

    user_role integration and sign_up and sign_in styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :password , format: { with: /^(?=.*[A-Z])(?=.*[$@$!%*?&])/ , :multiline => true , message: "Must contain atleast One Special-Character, One Upper-Case, and Minimum-Length of 8"}
+          enum role: [:Artist , :Manager]
+
+          validates :password , format: { with: /^(?=.*[A-Z])(?=.*[$@$!%*?&])/ , :multiline => true , message: "Must contain atleast One Special-Character, One Upper-Case, and Minimum-Length of 8"},
+           if: -> { password.present? }
+
+
 end
