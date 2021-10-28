288cc96941cfb61ac04a8e3614ee5d12154ffc72
muhammad-saadi <muhammad.saad@square63.org>
2021-10-28 16:46:17 -0700

    remove indentation errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-          enum role: [:Artist , :Manager]
+  enum role: [:Artist , :Manager]
 
-          validates :password , format: { with: /^(?=.*[A-Z])(?=.*[$@$!%*?&])/ , :multiline => true , message: "Must contain atleast One Special-Character, One Upper-Case, and Minimum-Length of 8"},
-           if: -> { password.present? }
-
-
+  validates :password , format: { with: /^(?=.*[A-Z])(?=.*[$@$!%*?&])/ , :multiline => true , message: "Must contain atleast One Special-Character, One Upper-Case, and Minimum-Length of 8"}, if: -> { password.present? }
 end
